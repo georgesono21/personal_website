affbd5f5c94105c177b0ddb8b7a1810e44e9d929
Yoshinobu Sono <ysono@ucsc.edu>
2022-09-17 01:52:54 -0700

    added skills and projects sections

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:invert_colors/invert_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "George Sono's Website",
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "George Sono's Website"),
     );
   }
 }
@@ -78,110 +79,291 @@ class _MyHomePageState extends State<MyHomePage>
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final currentWidth = MediaQuery.of(context).size.width;
+    final mobileWidth = 800;
+
     return Scaffold(
-        backgroundColor: Colors.blueGrey[800],
+        backgroundColor:
+            currentWidth < mobileWidth ? Colors.grey[900] : Colors.grey[900],
         body: Padding(
-          padding: const EdgeInsets.all(50),
-          child: Center(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              physics: NeverScrollableScrollPhysics(),
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Column(children: <Widget>[
-                  Text("Hello and welcome to my personal website! I am",
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                  SlideTransition(
-                    position: offsetAnimation,
-                    child: Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          Text("George Sono",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 100,
-                                fontWeight: FontWeight.bold,
-                              )),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Text("(曽野良信)",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "Currently, I am a second-year Computer Science student at UC Santa Cruz!",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Center(
+          padding: const EdgeInsets.all(0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: NeverScrollableScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                heightFactor: 1,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 80),
+                    Text("Hello and welcome to my personal website! I am",
+                        style: TextStyle(color: Colors.white, fontSize: 20)),
+                    SlideTransition(
+                      position: offsetAnimation,
+                      child: Padding(
+                        padding: EdgeInsets.all(30),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
+                          children: const <Widget>[
+                            Text("George Sono",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 100,
+                                  fontWeight: FontWeight.bold,
+                                )),
                             Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: InkWell(
-                                  //creates hyperlink text to website
-                                  child: Image.network(
-                                    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png',
-                                    width: 50,
-                                    height: 50,
-                                  ),
-                                  onTap: () {
-                                    launch(
-                                        "https://www.linkedin.com/in/yoshinobu-sono/");
-                                  }),
+                              padding: EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Text("(曽野良信)",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  )),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: InkWell(
-                                  //creates hyperlink text to website
-                                  child: Image.network(
-                                    'https://cdn-icons-png.flaticon.com/512/25/25231.png',
-                                    width: 50,
-                                    height: 50,
-                                  ),
-                                  onTap: () {
-                                    launch("https://github.com/georgesono21");
-                                  }),
-                            )
                           ],
                         ),
-                      )),
-                  Column(
-                    children: [
+                      ),
+                    ),
+                    Text(
+                      "Currently, I am a second-year Computer Science student at UC Santa Cruz!",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Links:",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontStyle: FontStyle.italic)),
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: InkWell(
+                                    //creates hyperlink text to website
+                                    child: Image.network(
+                                      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png',
+                                      width: 50,
+                                      height: 50,
+                                    ),
+                                    onTap: () {
+                                      launch(
+                                          "https://www.linkedin.com/in/yoshinobu-sono/");
+                                    }),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: InkWell(
+                                    //creates hyperlink text to website
+                                    child: InvertColors(
+                                      child: Image.network(
+                                        'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      launch("https://github.com/georgesono21");
+                                    }),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: InkWell(
+                                    //creates hyperlink text to website
+                                    child: Image.network(
+                                      'https://t3.ftcdn.net/jpg/03/77/85/04/360_F_377850455_Gk0rRBzegH6YX9SZK9YbgyYyLwrVb9zi.jpg',
+                                      width: 50,
+                                      height: 50,
+                                    ),
+                                    onTap: () {
+                                      launch(
+                                          "https://docs.google.com/document/d/149jjzXpJzzGJ08tuEioL2UVYTqkGEsHJ/edit?usp=sharing&ouid=104105135239584765519&rtpof=true&sd=true");
+                                    }),
+                              ),
+                            ],
+                          ),
+                        )),
+                    Column(
+                      children: const <Widget>[
+                        Text(
+                          "About Me",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              decoration: TextDecoration.underline),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: SizedBox(
+                            width: 750,
+                            child: Text(
+                              "I am an aspiring software engineer seeking internships for 2023 Spring/Summer. Moving from Japan, I came to the United States for better oppurtuniies in the field of tech. With learning about economics and finance as one of my primary hobbies, I aim to work in the financial tech industry with IT.",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Skills",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              decoration: TextDecoration.underline),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              children: const [
+                                Text("Programming:",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    )),
+                                SizedBox(height: 10),
+                                Text(
+                                    "- Python\n- Dart\n- Flutter\n- RISC-V Assembly\n- Javascript\n- HTML/CSS",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ))
+                              ],
+                            ),
+                            SizedBox(width: 70),
+                            Column(
+                              children: const [
+                                Text("Tools & Software:",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    )),
+                                SizedBox(height: 10),
+                                Text(
+                                    "- Git\n- Microsoft Excel\n- Microsoft Office",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15))
+                              ],
+                            ),
+                            SizedBox(width: 70),
+                            Column(
+                              children: const [
+                                Text("Human Languages:",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    )),
+                                SizedBox(height: 10),
+                                Text(
+                                    "- English (native)\n- Japanese (conversational)",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15))
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Column(children: <Widget>[
+                      //projects column
                       Text(
-                        "About Me",
+                        "Personal Projects",
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 30,
                             decoration: TextDecoration.underline),
                       ),
+                      SizedBox(height: 12),
                       Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
                         child: SizedBox(
                           width: 750,
-                          child: Text(
-                            "I am an aspiring software engineer seeking internships for 2023 Spring/Summer. Moving from Japan, I came to the United States for better oppurtuniies in the field of tech. With learning about economics and finance as one of my primary hobbies, I aim to work in the financial tech industry with IT.",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 700,
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: const [
+                                        Text("PlateBuddy",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 25,
+                                            )),
+                                      ],
+                                    ),
+                                    SizedBox(height: 15),
+                                    Text(
+                                      "- Developed a webapp for home gyms that calculates the weight needed on each side of a barbell \n- Provides what weights are possible, given input by the type and amount of plates the user has in their gym \n- UI/UX made in Flutter (Dart) and scripts are written in Python (using dynamic programming)",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
+                        child: SizedBox(
+                          width: 750,
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 700,
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: const [
+                                        Text("Stock Trading Bot",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 25,
+                                            )),
+                                      ],
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      "- Created a Python trading program with the Alpaca API and Python\n- Executes buy and sell orders (will eventually incorporate Technical Analysis) \n- Program was tested against a real time simulation of the NYSE",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       )
-                    ],
-                  )
-                ]),
+                    ])
+                  ]),
+                ),
               ),
             ),
           ),
